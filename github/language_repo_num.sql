739a30d6337f574a4a00a3e9d716c7b57a0d41ca
hiroki1117 <scala1117@gmail.com>
2020-10-30 23:02:26 +0900

    add EXP

--- github/language_repo_num.sql
+++ github/language_repo_num.sql
@@ -7,3 +7,20 @@ select name, count(1) as reponum
 from tmp
 group by name
 order by reponum desc
+
+
+/*
+Githubの言語のリポジトリ数ランク
+一リポジトリで複数言語という場合もある
+
+1	JavaScript 1107589
+2	CSS 820632
+3	HTML 785340
+4	Shell 641469
+5	Python 551265
+6	Ruby 377111
+7	Java 371326
+8	PHP 342832
+9	C 293987
+10	C++ 280422
+*/
