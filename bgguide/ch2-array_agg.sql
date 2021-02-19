f0fd1060456e8ca15ab871e0d3fee3b2300c7edc
hiroki1117 <scala1117@gmail.com>
2021-02-19 16:11:05 +0900

    add

---
+++ bgguide/ch2-array_agg.sql
@@ -0,0 +1,16 @@
+-- 配列の中をソートできる
+SELECT 
+  gender
+  , ARRAY_AGG(numtrips order by year) AS numtrips
+FROM (
+  SELECT
+    gender
+    , EXTRACT(YEAR from starttime) AS year
+    , COUNT(1) AS numtrips
+  FROM
+    `bigquery-public-data`.new_york_citibike.citibike_trips
+  WHERE gender != 'unknown' and starttime IS NOT NULL
+  GROUP BY gender, year
+  HAVING year > 2016
+)
+GROUP BY gender
